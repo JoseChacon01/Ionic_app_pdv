6b21f1587d4be07d2950d2c88e964ef5927d30f4
Jose Moreira Chacon <josemoreirachacon06@gmail.com>
2022-11-24 12:36:46 -0300

    Trabalhando com API e mensagens

--- src/app/add-usuario/add-usuario.page.ts
+++ src/app/add-usuario/add-usuario.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 import { promise } from 'protractor';
 import { Api } from 'src/services/api';
 
@@ -20,16 +21,29 @@ export class AddUsuarioPage implements OnInit {
   constructor(
     private router:Router, 
     private provider: Api, //importando a class Apis que esta dentro da pasta "Services"
-    private actRouter:ActivatedRoute //isso é para que seja possivel passar parametros entre tela (passar de uma pagina para outra 
-
+    private actRouter:ActivatedRoute, //isso é para que seja possivel passar parametros entre tela (passar de uma pagina para outra 
+    public toastController: ToastController //Formato, segundo a documentação para tratar notificações ->  Ele pode ser usado para fornecer feedback sobre uma operação ou para exibir uma mensagem do sistema. 
     ) { } 
 
-  // funções 
-  ngOnInit() {                                    //estamos utilizando o actRouter para passar parametros do tipo dados (data) que podem ser qualquer valor (any), sejam eles int, string, etc. -> Utilizamo o "this."" pq o ele mento "actRouter" estava fora, para chamar ele precisamos disso.
+  // funções   //Função execultada ao iniciar a pagina
+  ngOnInit() {    
+   // this.mensagemSucesso(); //Ao iniciar a pagina ADD-USUARIOS a msg de sucesso irá aparecer - TESTE                                              //estamos utilizando o actRouter para passar parametros do tipo dados (data) que podem ser qualquer valor (any), sejam eles int, string, etc. -> Utilizamo o "this."" pq o ele mento "actRouter" estava fora, para chamar ele precisamos disso.
     this.actRouter.params.subscribe((data:any)=>{ // Quando iremos passar parametros, por exemplo? Quando formos editar um dado, quando acessarmos a tela de "edição" é necessario que os dados de edição venham como parametros, de algum lugar. Ou senha, os campos devem vir preenchido, para podermos altera-los.
 
     });
   }
+
+  //async: Força que seja execultado apenas uma vez por chamada
+ async mensagem(mensagem, cor){
+    const toast = await this.toastController.create({ // É criada a constaente "const" segundo a documentação e passado algumas propriedades como a mensagem, duração, botões, cores, etc.
+      message: mensagem,
+      duration:2000,
+      color: cor
+    });
+    toast.present();
+
+  }
+
 
   Usuarios(){
     this.router.navigate(['usuarios'])
@@ -43,8 +57,19 @@ export class AddUsuarioPage implements OnInit {
         email: this.email,
         senha: this.senha,
         nivel: this.nivel,
-      }
-      this.provider.dadosApi(dados, 'usuarios/inserir.php' ) // chamando a função "dadosApi" que se encontra em "api.ts"-> Passamos para "dadosApi" 2 parametros, que são as informções que estamos passando (dados -> Variavel que foi inserida a cima) e a String da Api (ou seja, a rota do arqui responsavel pela ponte da inserção dos dados no BD)
+      }                                                                // chamando a função "dadosApi" que se encontra em "api.ts"-> Passamos para "dadosApi" 2 parametros, que são as informções que estamos passando (dados -> Variavel que foi inserida a cima) e a String da Api (ou seja, a rota do arqui responsavel pela ponte da inserção dos dados no BD)        
+      this.provider.dadosApi(dados, 'usuarios/inserir.php' ).subscribe(
+        data=>{
+          
+          if(data['ok'] == true){          //Se tiver tudo certo no preenchimento do formulario 'ok' o usuario sera direcionado(navigate) para pagina "usuarios" e a mensagem definida em "inserir.php" ira receber a cor "suceess".
+          this.router.navigate(['usuarios']);
+          this.mensagem(data['mensagem'], 'success');
+        }else{
+          this.mensagem(data['mensagem'], 'danger'); //Caso não seja prenchido corretamente, ira ser exibida a mensagem definida em "inserir.php" na cor "danger"
+        }
+
+        }
+      ) 
     }) //TUDO ISSO AQUI(DENTRO DE CADASTRAR), QUE FOI DESCRITO ATÉ AGORA, REFERE-SE A FUNÇÃO DE CADASTRAR = Quer dizer que, quando o usuario abrir o formulario, ele vai poder se cadastrar e esses dados vão ser armazenados no BD.
   }
 
